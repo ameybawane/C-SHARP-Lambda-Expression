60410a405b1d202fee645b9e920a905838b823dc
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2023-05-03 18:10:39 +0530

    Add files via upload

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        public delegate string GreetingsDelegate(string name);
+        static void Main(string[] args)
+        {
+            // // Anonymous/Nameless Method
+            //GreetingsDelegate obj = delegate (string name)
+            //{
+            //    return "Hello " + name + " a very good morning.";
+            //};
+
+            // lambda expression
+            GreetingsDelegate obj = (name) =>
+            {
+                return "Hello " + name + " a very good morning.";
+            };
+
+            Console.WriteLine(obj.Invoke("Amey"));
+            Console.ReadKey();
+        }
+    }
+}
